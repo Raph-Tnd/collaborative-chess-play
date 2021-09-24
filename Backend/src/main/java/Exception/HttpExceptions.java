a3f8300cbeb6cde20f7f7d6c98da5beb211672a2
Louis NORMAND <louis.normand@etu.univ-nantes.fr>
2021-09-24 18:58:22 +0200

    correction message retour erreur http

--- Backend/src/main/java/Exception/HttpExceptions.java
+++ Backend/src/main/java/Exception/HttpExceptions.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @ControllerAdvice
 public class HttpExceptions {
 
     @ExceptionHandler(value = ExceptionUserAlreadyConnected.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ResponseEntity<String> handleUserAlreadyConnectedException(ExceptionUserAlreadyConnected exception) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    public void handleUserAlreadyConnectedException(ExceptionUserAlreadyConnected exception, HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.CONFLICT.value(), exception.getMessage());
     }
 }
