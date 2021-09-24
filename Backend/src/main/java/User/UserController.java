5de9414a660e483c26de781f8eaa05175a0555a7
Louis NORMAND <louis.normand@etu.univ-nantes.fr>
2021-09-24 18:58:22 +0200

    correction message retour erreur http

--- Backend/src/main/java/User/UserController.java
+++ Backend/src/main/java/User/UserController.java
@@ -9,10 +9,10 @@ public class UserController {
     @GetMapping("/game/{id}")
     public String movePiece(@PathVariable int id){
         return "Moved piece n°" + id;
-    }
+    } //todo
 
     @PostMapping("/game/vote")
     public String voteMove(@RequestBody MoveModel moveModel) {
-        return "Voted for move piece n°" + moveModel.pieceId + " to " + moveModel.xCoord + "," + moveModel.yCoord;
+        return "Voted for move piece n°" + moveModel.pieceId + " to " + moveModel.xCoord + "," + moveModel.yCoord; //todo
     }
 }
