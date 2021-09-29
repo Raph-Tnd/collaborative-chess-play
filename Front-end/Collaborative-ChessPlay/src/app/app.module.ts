5618bff7a7cbc174a448d84a82f2e8e949834e1d
thibault <thibault.colin@etu.univ-nantes.fr>
2021-09-29 10:07:58 +0200

    Front-end page d'arrivée 1ere version

--- Front-end/Collaborative-ChessPlay/src/app/app.module.ts
+++ Front-end/Collaborative-ChessPlay/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CcpCadreCentralPpComponent } from './ccp-cadre-central-pp/ccp-cadre-central-pp.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CcpCadreCentralPpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
