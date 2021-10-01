07f590a6d206f45f6888c371f4c151397c031221
Raphael TENAUD <raphael.tenaud@etu.univ-nantes.fr>
2021-10-01 16:16:43 +0200

    Test mat angular

--- Front-end/Collaborative-ChessPlay/src/app/app.module.ts
+++ Front-end/Collaborative-ChessPlay/src/app/app.module.ts
@@ -7,18 +7,25 @@ import { CcpCadreCentralPpComponent } from './ccp-cadre-central-pp/ccp-cadre-cen
 import { FormsModule } from '@angular/forms';
 import { ChessBoardComponent } from './chess-board/chess-board.component';
 import { PieceComponent } from './piece/piece.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     CcpCadreCentralPpComponent,
     ChessBoardComponent,
-    PieceComponent
+    PieceComponent,
+    MatFormField
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front-end/Collaborative-ChessPlay/src/app/chess-board/chess-board.component.ts
+++ Front-end/Collaborative-ChessPlay/src/app/chess-board/chess-board.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-chess-board',
@@ -8,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class ChessBoardComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  moveField : string = "";
 
   BOARD: string[][] = [
     ["Tn1","Cn1","Fn1","Dn","Rn","Fn2","Cn2","Tn2"],
@@ -19,7 +23,6 @@ export class ChessBoardComponent implements OnInit {
     ["Pb1","Pb2","Pb3","Pb4","Pb5","Pb6","Pb7","Pb8"],
     ["Tb1","Cb1","Fb1","Db","Rb","Fb2","Cb2","Tb2"]
   ]
-
   isEven(n: number) {
     return n % 2 == 0;
   }
@@ -41,6 +44,22 @@ export class ChessBoardComponent implements OnInit {
         return "beige";
       }
     }
+  }
+
+
+
+  moveFormControl = new FormControl('',[
+    Validators.required,
+    Validators.pattern("^[a-hA-H][0-8][a-hA-H][0-8]$")
+  ])
+
+  parseMove(move : string){
+
+  }
+
+  onEnter(){
+    console.log(this.moveField);
+    this.moveField = "";
   }
 
 
