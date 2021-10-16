49190e8447ecd7f43224a64e6ac87218daff9b64
Louis Normand <louis.normand@etu.univ-nantes.fr>
2021-10-16 23:30:47 +0200

    spring data initial test value

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,3 +1,5 @@
+--create table if not exists player(id_game int,w_players varchar(255),b_players varchar(255));
+--create table if not exists game(id varchar(255),name varchar(255),team boolean);
 insert into player values ("DummyGame", "p1", 0);
 insert into player values ("DummyGame", "p2", 0);
 insert into player values ("DummyGame", "p3", 1);
