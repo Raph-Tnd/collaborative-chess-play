c39f27ddcb82fbddc65d48f19afdc3c87e55808d
Louis NORMAND <louis.normand@etu.univ-nantes.fr>
2021-12-18 21:00:05 +0100

    fix git issue backend

--- Backend/src/main/java/WebSocket/WebSocketController.java
+++ Backend/src/main/java/WebSocket/WebSocketController.java
@@ -1,9 +1,7 @@
 package main.java.WebSocket;
 
-import main.java.Data.Model.MoveModel;
-import main.java.Exception.ExceptionNotPlayerTurn;
-import main.java.Exception.ExceptionUserAlreadyPlayed;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class WebSocketController {
-    final String prefixe = "/game";
 
-    @SubscribeMapping(value = (prefixe + "/vote"))
-    public void voteMove(@RequestBody MoveModel moveModel) throws InterruptedException, ExceptionUserAlreadyPlayed, ExceptionNotPlayerTurn {
-        //TODO implémenter le vote pour les webs sockets
+    @MessageMapping(value = "/test")
+    @SendTo("/chat/private")
+    public String send(@RequestBody String message) {
+        System.out.println("WR recue : " + message);
+        return "Hello " + message +" !";
     }
 }
