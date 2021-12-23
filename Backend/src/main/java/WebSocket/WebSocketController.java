9c4aa3bbfd00ed7a5c06d93b082ddef06541f7b0
Louis NORMAND <louis.normand@etu.univ-nantes.fr>
2021-12-23 14:48:05 +0100

    get game info websocket

--- Backend/src/main/java/WebSocket/WebSocketController.java
+++ Backend/src/main/java/WebSocket/WebSocketController.java
@@ -1,14 +1,18 @@
 package main.java.WebSocket;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import main.java.Data.Model.GameModel;
 import main.java.Data.Model.MoveModel;
+import main.java.Data.Model.PlayerModel;
 import main.java.Exception.ExceptionGameDoesNotExist;
 import main.java.Exception.ExceptionNotPlayerTurn;
 import main.java.Exception.ExceptionUserAlreadyPlayed;
 import main.java.Game.GameService;
+import main.java.User.UserService;
 import org.dom4j.rule.Mode;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,18 +20,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.JSONObject;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class WebSocketController {
 
     @Autowired
     private GameService gameService;
+    @Autowired
+    private UserService userService;
 
     @MessageMapping(value = "/test")
     @SendTo("/chat/private")
     public String send(@RequestBody String message) {
         System.out.println("WR recue : " + message);
         return "Hello " + message +" !";
+    }
+
+    @MessageMapping(value = "/connectToGame/{idGame}")
+    @SendTo("/chat/getGameInfo/{idGame}")
+    public List<PlayerModel> sendGameInfo(@DestinationVariable String id) {
+        return userService.getAllPlayer(id);
     }
 
     @MessageMapping(value = "/submitMove/{idGame}")
